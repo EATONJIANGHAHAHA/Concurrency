b6f10fe8240affdb250af299d7f6ef63978b259c
EATONJIANGHAHAHA <eatonjiangtonlei@gmail.com>
2018-12-30 20:14:25 +0800

    ?

---
+++ src/main/java/com/mmall/concurrency/example/threadPool/ThreadPoolExample1.java
@@ -0,0 +1,29 @@
+package com.mmall.concurrency.example.threadPool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class ThreadPoolExample1 {
+
+    public static void main(String[] args) {
+
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(index * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cachedThreadPool.execute(new Runnable() {
+                public void run() {
+                    log.info("{}", index);
+                }
+            });
+        }
+        cachedThreadPool.shutdown();
+    }
+}
